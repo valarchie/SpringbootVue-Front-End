86fd9872b85631d882b97d6cb8f666a7cb53eb26
valarchie <343928303@qq.com>
2022-08-28 22:13:20 +0800

    重构配置模块

--- src/api/system/config.js
+++ src/api/system/config.js
@@ -25,22 +25,6 @@ export function getDicts(dictType) {
   });
 }
 
-// 根据参数键名查询参数值 TODO 弃用了
-export function getConfigKey(configKey) {
-  return request({
-    url: `/system/config/configKey/${configKey}`,
-    method: 'get',
-  });
-}
-
-// 新增参数配置
-export function addConfig(data) {
-  return request({
-    url: '/system/config',
-    method: 'post',
-    data,
-  });
-}
 
 // 修改参数配置
 export function updateConfig(data) {
@@ -51,13 +35,6 @@ export function updateConfig(data) {
   });
 }
 
-// 删除参数配置
-export function delConfig(configId) {
-  return request({
-    url: `/system/config/${configId}`,
-    method: 'delete',
-  });
-}
 
 // 刷新参数缓存
 export function refreshCache() {
